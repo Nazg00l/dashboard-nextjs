d9941120e845dd90d016c4fa42de13ece3153e49
nazg00l <malaz511@gmail.com>
2024-07-07 01:20:11 +0300

    Adding pagination

--- app/(main)/posts/page.tsx
+++ app/(main)/posts/page.tsx
@@ -1,10 +1,12 @@
 import PostsTable from "@/components/posts/PostsTable";
 import BackButton from "@/components/BackButton";
+import PostsPagination from "@/components/posts/PostsPagination";
 
 const PostsPage = () => {
     return <>
         <BackButton text="Go Back" link="/" />
         <PostsTable />
+        <PostsPagination />
     </>;
 }
 
